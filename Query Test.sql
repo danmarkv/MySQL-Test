ed8e2a2945ea4d9cd9a9769273678703bb0a82e8
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-15 17:35:37 +0800

    using the BETWEEN operator

--- Query Test.sql
+++ Query Test.sql
@@ -1,8 +1,7 @@
 SELECT *
-FROM products
-WHERE quantity_in_stock IN (49, 38, 72)
+FROM customers
+WHERE birth_date BETWEEN '1990-01-01' AND '2000-01-01'
 
 -- SELECT *
 -- FROM customers
--- WHERE state IN ('VA', 'FL', 'GA')
-
+-- WHERE points BETWEEN 1000 AND 3000
