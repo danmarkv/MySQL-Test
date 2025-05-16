d59ab0d639d2671c0720df7862ddd6ceb8a00c00
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-16 23:44:35 +0800

    using the REGEXP operator

--- Query Test.sql
+++ Query Test.sql
@@ -1,16 +1,19 @@
 SELECT *
 FROM customers
-WHERE phone LIKE '%9'
--- WHERE phone LIKE '%9' -> displays all number not ending with 9
+-- WHERE first_name REGEXP 'elka|ambur' -- OR first_name REGEXP 'ambur'  -- use elka|ambur
+-- WHERE last_name REGEXP 'ey$|on$' -- OR last_name REGEXP 'on$' -- use |
+WHERE last_name REGEXP '^my|se' -- OR last_name REGEXP 'se' use |
+-- WHERE last_name REGEXP 'b[ru]' -- or use br|bu
 
 -- SELECT *
 -- FROM customers
--- WHERE address LIKE '%TRAIL%' OR 
--- 	  address LIKE '%AVENUE%'
+-- WHERE last_name REGEXP '[gim]e'
+-- WHERE last_name REGEXP 'field|mac' -> finding multiple words
+-- WHERE last_name LIKE '%field' -> the same REGEXP 'field'
 
--- SELECT *
--- FROM customers
--- WHERE last_name LIKE 'b____y'
-
--- % any number of characters
--- _ single character
+-- ^ beginning
+-- $ end
+-- | logical or
+-- [abcd] match any single character listed inside
+-- [a-h] ranged of characters
+ 
