2e6c72254bc99417b4ccf993b88e1f2398e86f28
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-16 23:49:23 +0800

    using the NULL operator

--- Query Test.sql
+++ Query Test.sql
@@ -1,19 +1,8 @@
 SELECT *
-FROM customers
--- WHERE first_name REGEXP 'elka|ambur' -- OR first_name REGEXP 'ambur'  -- use elka|ambur
--- WHERE last_name REGEXP 'ey$|on$' -- OR last_name REGEXP 'on$' -- use |
-WHERE last_name REGEXP '^my|se' -- OR last_name REGEXP 'se' use |
--- WHERE last_name REGEXP 'b[ru]' -- or use br|bu
+FROM orders
+WHERE shipped_date IS NULL
 
 -- SELECT *
 -- FROM customers
--- WHERE last_name REGEXP '[gim]e'
--- WHERE last_name REGEXP 'field|mac' -> finding multiple words
--- WHERE last_name LIKE '%field' -> the same REGEXP 'field'
-
--- ^ beginning
--- $ end
--- | logical or
--- [abcd] match any single character listed inside
--- [a-h] ranged of characters
+-- WHERE phone IS NULL -- IS NOT NULL to show all customers with phone numbers
  
