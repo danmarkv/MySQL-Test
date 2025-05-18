b7e01f0d92ac29fc509690ea7db9a979d5000719
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-19 03:41:06 +0800

    joining multiple tables

--- Query Test.sql
+++ Query Test.sql
@@ -1,10 +1,32 @@
-USE sql_hr;
+USE sql_invoicing;
 
 SELECT
-	e.employee_id,
-    e.first_name,
-    m.first_name AS manager
-FROM employees e
-JOIN employees m
-	ON e.reports_to = m.employee_id
+	p.date,
+    p.invoice_id,
+    p.amount,
+    c.name,
+    pm.name
+-- 	c.client_id,
+--     c.name,
+--     p.date,
+--     pm.name
+FROM payments p
+JOIN clients c
+	ON p.client_id = c.client_id
+JOIN payment_methods pm
+	ON p.payment_method = pm.payment_method_id
+
+-- USE sql_store;
+
+-- SELECT
+-- 	o.order_id,
+--     o.order_date,
+--     c.first_name,
+--     c.last_name,
+--     os.name AS status
+-- FROM orders o
+-- JOIN customers c
+-- 	ON o.customer_id = c.customer_id
+-- JOIN order_statuses os
+-- 	ON o.status = os.order_status_id
  
