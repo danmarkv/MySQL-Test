b850b52afc40f77b25b77b95fbf7dd758d9981f9
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-18 12:39:01 +0800

    using INNER JOIN

--- Query Test.sql
+++ Query Test.sql
@@ -1,11 +1,10 @@
-SELECT *
-FROM customers
-ORDER BY points DESC
-LIMIT 3
+SELECT order_id, p.product_id, name, quantity, o.unit_price
+FROM products p
+JOIN order_items o
+	ON p.product_id = o.product_id
 
--- The order of the clauses should be SELECT, FROM, WHERE, ORDER BY, LIMIT
-
--- SELECT *
--- FROM customers
--- LIMIT 6, 3
+-- SELECT order_id, o.customer_id, first_name, last_name
+-- FROM orders o
+-- JOIN customers c
+-- 	ON o.customer_id = c.customer_id
  
