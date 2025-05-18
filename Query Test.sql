5cd7afafaaf52fb577a30731224b83f700a5c1fc
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-18 11:47:28 +0800

    using the ORDER BY, DESC operator

--- Query Test.sql
+++ Query Test.sql
@@ -1,7 +1,7 @@
-SELECT order_id, product_id, quantity, unit_price
+SELECT *, quantity * unit_price AS total_price
 FROM order_items
 WHERE order_id = 2
-ORDER BY product_id
+ORDER BY total_price DESC
 
 -- SELECT first_name, last_name, 10 AS points
 -- FROM customers
