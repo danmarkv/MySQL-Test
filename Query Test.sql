42c3002b977daf8a459cbef8abbf14daf7082b9e
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-19 23:28:30 +0800

    using USING clause

--- Query Test.sql
+++ Query Test.sql
@@ -1,9 +1,21 @@
-USE sql_hr;
-
 SELECT
-	e.employee_id,
-	e.first_name,
-    m.first_name AS manager
-FROM employees e
-LEFT JOIN employees m
-	ON e.reports_to = m.employee_id
+	p.date,
+    c.name AS client,
+    p.amount,
+    pm.name AS payment_method
+FROM clients c
+JOIN payments p
+	USING (client_id)
+JOIN payment_methods pm
+	ON p.payment_method = pm.payment_method_id
+
+-- SELECT 
+-- 	o.order_id,
+-- 	c.first_name,
+--     sh.name AS shipper
+-- FROM orders o
+-- JOIN customers c
+-- -- 	ON o.customer_id = c.customer_id
+-- 	USING (customer_id)
+-- LEFT JOIN shippers sh
+-- 	USING (shipper_id)
