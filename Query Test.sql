de8a8c2fc5149ffeee8962144a814fa63e998c17
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-20 03:35:21 +0800

    inserting a row

--- Query Test.sql
+++ Query Test.sql
@@ -1,46 +1,28 @@
-SELECT
-	customer_id,
-    first_name,
-    points,
-    'Bronze' AS type
-FROM customers
-WHERE points < 2000
-UNION
-SELECT
-	customer_id,
-    first_name,
-    points,
-    'Silver' AS type
-FROM customers
-WHERE points >= 2000 AND points <= 3000 -- can also use WHERE points BETWEEN 2000 AND 3000
-UNION
-SELECT
-	customer_id,
-    first_name,
-    points,
-    'Gold' AS type
-FROM customers
-WHERE points > 3000
-ORDER BY first_name
-	
--- SELECT
--- 	first_name
--- FROM customers
--- UNION
--- SELECT
--- 	name
--- FROM shippers
+INSERT INTO customers (
+	first_name,
+    last_name,
+    birth_date,
+    address,
+    city,
+    state)
+VALUES (
+    'John', 
+    'Smith', 
+    '1990-01-01',
+    'address',
+    'city',
+    'CA')
 
--- SELECT
--- 	order_id,
---     order_date,
---     'Active' AS status
--- FROM orders
--- WHERE order_date > '2019-01-01'
--- UNION
--- SELECT
--- 	order_id,
---     order_date,
---     'Archive' AS status
--- FROM orders
--- WHERE order_date < '2019-01-01'
+-- can also use above
+
+-- INSERT INTO customers
+-- VALUES (
+-- 	DEFAULT, 
+--     'John', 
+--     'Smith', 
+--     '1990-01-01',
+--     NULL,
+--     'address',
+--     'city',
+--     'CA',
+--     DEFAULT)
