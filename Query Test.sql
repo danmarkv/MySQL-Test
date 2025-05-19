9f587f0f41ba4a4c38ff6dcdbc2801a75894656e
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-19 22:31:11 +0800

    understanding the implicit join syntax

--- Query Test.sql
+++ Query Test.sql
@@ -1,5 +1,12 @@
 SELECT *
-FROM order_items oi
-JOIN order_item_notes oin
-	ON oi.order_id = oin.order_id
-    AND oi.product_id = oin.product_id
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id
+  
+-- Implicit Join Syntax
+-- SELECT *
+-- FROM orders o, customers c
+-- WHERE o.customer_id = c.customer_id
+
+-- BEWARE OF IMPLICIT JOIN SYNTAX because it can cause CROSS JOIN
+-- BETTER TO USE EXPLICIT JOIN SYNTAX
