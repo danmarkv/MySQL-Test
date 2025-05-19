50752adfafce50d5cb329b492acad49094f8912e
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-19 23:37:51 +0800

    using explicit and implicit CROSS JOIN

--- Query Test.sql
+++ Query Test.sql
@@ -1,21 +1,19 @@
+-- IMPLICIT CROSS JOIN
 SELECT
-	p.date,
-    c.name AS client,
-    p.amount,
-    pm.name AS payment_method
-FROM clients c
-JOIN payments p
-	USING (client_id)
-JOIN payment_methods pm
-	ON p.payment_method = pm.payment_method_id
+	p.name AS products,
+    sh.name AS shipper
+FROM products p, shippers sh
 
--- SELECT 
--- 	o.order_id,
--- 	c.first_name,
+-- EXPLICIT CROSS JOIN
+-- SELECT
+-- 	p.name AS products,
 --     sh.name AS shipper
--- FROM orders o
--- JOIN customers c
--- -- 	ON o.customer_id = c.customer_id
--- 	USING (customer_id)
--- LEFT JOIN shippers sh
--- 	USING (shipper_id)
+-- FROM products p
+-- CROSS JOIN shippers sh
+
+-- SELECT
+-- 	c.first_name AS customer,
+--     p.name AS product
+-- FROM customers c -- adding (, products p) is IMPLICIT CROSS JOIN
+-- CROSS JOIN products p -- EXPLICIT CROSS JOIN
+-- ORDER BY c.first_name
