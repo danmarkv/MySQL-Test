5c0c25dc1a0988f0b11f256c55d26d248fa3e886
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-19 22:48:06 +0800

    using OUTER JOIN

--- Query Test.sql
+++ Query Test.sql
@@ -1,12 +1,22 @@
-SELECT *
-FROM orders o
-JOIN customers c
-	ON o.customer_id = c.customer_id
-  
--- Implicit Join Syntax
--- SELECT *
--- FROM orders o, customers c
--- WHERE o.customer_id = c.customer_id
+SELECT
+	p.product_id,
+    p.name,
+    oi.quantity
+FROM products p
+LEFT JOIN order_items oi
+	ON p.product_id = oi.product_id
 
--- BEWARE OF IMPLICIT JOIN SYNTAX because it can cause CROSS JOIN
--- BETTER TO USE EXPLICIT JOIN SYNTAX
+-- LEFT JOIN shows the all the records in the LEFT table, which is the table after the FROM clause, whether the ON condition is true or not
+-- RIGHT JOIN shows the all the records in the RIGHT table, which is the afte the JOIN clause
+-- OUTER is option if using LEFT/RIGHT JOIN
+-- INNER is optional if using JOIN
+
+-- SELECT
+-- 	c.customer_id,
+--     c.first_name,
+--     o.order_id
+-- FROM customers c
+-- LEFT JOIN orders o
+-- 	ON c.customer_id = o.customer_id
+-- ORDER BY c.customer_id
+
