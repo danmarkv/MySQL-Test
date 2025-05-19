7a7a23e32c86b3a20a5e2aea1dca44555e31aa0e
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-19 22:26:11 +0800

    compound join conditions

--- Query Test.sql
+++ Query Test.sql
@@ -1,32 +1,5 @@
-USE sql_invoicing;
-
-SELECT
-	p.date,
-    p.invoice_id,
-    p.amount,
-    c.name,
-    pm.name
--- 	c.client_id,
---     c.name,
---     p.date,
---     pm.name
-FROM payments p
-JOIN clients c
-	ON p.client_id = c.client_id
-JOIN payment_methods pm
-	ON p.payment_method = pm.payment_method_id
-
--- USE sql_store;
-
--- SELECT
--- 	o.order_id,
---     o.order_date,
---     c.first_name,
---     c.last_name,
---     os.name AS status
--- FROM orders o
--- JOIN customers c
--- 	ON o.customer_id = c.customer_id
--- JOIN order_statuses os
--- 	ON o.status = os.order_status_id
- 
+SELECT *
+FROM order_items oi
+JOIN order_item_notes oin
+	ON oi.order_id = oin.order_id
+    AND oi.product_id = oin.product_id
