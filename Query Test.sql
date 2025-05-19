03a028c1fbe5a01c4d8b472dcd7a6786640cc120
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-19 23:09:22 +0800

    using OUTER JOIN on multiple tables

--- Query Test.sql
+++ Query Test.sql
@@ -1,22 +1,15 @@
 SELECT
-	p.product_id,
-    p.name,
-    oi.quantity
-FROM products p
-LEFT JOIN order_items oi
-	ON p.product_id = oi.product_id
-
--- LEFT JOIN shows the all the records in the LEFT table, which is the table after the FROM clause, whether the ON condition is true or not
--- RIGHT JOIN shows the all the records in the RIGHT table, which is the afte the JOIN clause
--- OUTER is option if using LEFT/RIGHT JOIN
--- INNER is optional if using JOIN
-
--- SELECT
--- 	c.customer_id,
---     c.first_name,
---     o.order_id
--- FROM customers c
--- LEFT JOIN orders o
--- 	ON c.customer_id = o.customer_id
--- ORDER BY c.customer_id
+    o.order_id,
+    o.order_date,
+    c.first_name,
+    sh.name AS shipper,
+    os.name AS status
+FROM customers c
+JOIN orders o
+	ON c.customer_id = o.customer_id
+LEFT JOIN shippers sh
+	ON o.shipper_id = sh.shipper_id
+JOIN order_statuses os
+	ON o.status = os.order_status_id
+ORDER BY c.customer_id
 
