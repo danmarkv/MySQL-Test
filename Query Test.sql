14d201feb7a9cb9a735c796b44fea23cd084e1a5
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-21 22:50:19 +0800

    updating a single row

--- Query Test.sql
+++ Query Test.sql
@@ -1,14 +1,4 @@
-CREATE TABLE invoice_archived AS
-SELECT
-	i.invoice_id,
-    i.number,
-    c.name,
-    i.invoice_total,
-    i.payment_total,
-    i.invoice_date,
-    i.due_date,
-    i.payment_date
-FROM invoices i
-LEFT JOIN clients c
-	ON i.client_id = c.client_id -- use USING (client_id)
-WHERE payment_date -- can also use (IS NOT NULL)
+UPDATE invoices
+SET payment_total = invoice_total * 0.5,
+	payment_date = due_date
+WHERE invoice_id = 3
