875312246cd55178787d4c8fb01ed4f4166658ae
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-21 22:36:28 +0800

    creating a copy of a table

--- Query Test.sql
+++ Query Test.sql
@@ -1,8 +1,14 @@
-INSERT INTO orders (customer_id, order_date, status)
-VALUES (1, '2019-01-02', 1);
-
-INSERT INTO order_items
-VALUES 
-	(LAST_INSERT_ID(), 1, 1, 2.95),
-    (LAST_INSERT_ID(), 2, 1, 3.95)
-    
+CREATE TABLE invoice_archived AS
+SELECT
+	i.invoice_id,
+    i.number,
+    c.name,
+    i.invoice_total,
+    i.payment_total,
+    i.invoice_date,
+    i.due_date,
+    i.payment_date
+FROM invoices i
+LEFT JOIN clients c
+	ON i.client_id = c.client_id -- use USING (client_id)
+WHERE payment_date -- can also use (IS NOT NULL)
