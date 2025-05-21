14f0d952a0efffbf5abc75e1fead71215b957ad2
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-21 21:55:38 +0800

    inserting heirarchical rows

--- Query Test.sql
+++ Query Test.sql
@@ -1,5 +1,8 @@
-INSERT INTO products (name, quantity_in_stock, unit_price)
-VALUE ('Product1', 11, 1.96),
-	  ('Product2', 12, 1.97),
-      ('Product3', 13, 1.98)
-		
+INSERT INTO orders (customer_id, order_date, status)
+VALUES (1, '2019-01-02', 1);
+
+INSERT INTO order_items
+VALUES 
+	(LAST_INSERT_ID(), 1, 1, 2.95),
+    (LAST_INSERT_ID(), 2, 1, 3.95)
+    
