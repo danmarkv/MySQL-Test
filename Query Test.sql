ba344e39e62d0827b00b132c818d93ef117e5a70
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-22 20:14:24 +0800

    deleting rows

--- Query Test.sql
+++ Query Test.sql
@@ -1,20 +1,6 @@
-
-UPDATE orders
-SET
-	comments = 'Gold Customer'
-WHERE customer_id IN 
-			(SELECT customer_id
-            FROM customers
-            WHERE points > 3000)
-
--- UPDATE invoices
--- SET 
--- 	payment_total = invoice_total * 0.5,
--- 	payment_date = due_date
--- WHERE payment_date IS NULL
---     
--- -- WHERE client_id IN 
--- -- 			(SELECT client_id
--- -- 			FROM clients
--- -- 			WHERE state IN ('CA', 'NY'))
+DELETE FROM invoices
+WHERE client_id = 
+			(SELECT *
+			FROM clients
+			WHERE name = 'Myworks')
 
