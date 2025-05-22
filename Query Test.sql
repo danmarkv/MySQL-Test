8e33c164df1a652bfe916419f7b53723ec094bdd
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-22 20:11:54 +0800

    using subqueries in updates

--- Query Test.sql
+++ Query Test.sql
@@ -1,10 +1,20 @@
-USE sql_store;
 
-UPDATE customers
-SET points = (points + 50)
-WHERE birth_date < '1990-01-01'
+UPDATE orders
+SET
+	comments = 'Gold Customer'
+WHERE customer_id IN 
+			(SELECT customer_id
+            FROM customers
+            WHERE points > 3000)
 
 -- UPDATE invoices
--- SET payment_total = invoice_total * 0.5,
+-- SET 
+-- 	payment_total = invoice_total * 0.5,
 -- 	payment_date = due_date
--- WHERE client_id = 3
+-- WHERE payment_date IS NULL
+--     
+-- -- WHERE client_id IN 
+-- -- 			(SELECT client_id
+-- -- 			FROM clients
+-- -- 			WHERE state IN ('CA', 'NY'))
+
