00662a8277540007634251931c372385d3984418
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-23 22:49:42 +0800

    using the GROUP BY clause

--- Query Test.sql
+++ Query Test.sql
@@ -1,29 +1,47 @@
 SELECT
-	'First half of 2019' AS date_range,
-	SUM(invoice_total) AS total_sales,
-    SUM(payment_total) AS total_payments,
-    SUM(invoice_total - payment_total) AS what_we_expect
-FROM invoices
-	WHERE invoice_date BETWEEN '2019-01-01' AND '2019-06-30'
-UNION
-SELECT
-	'Second half of 2019' AS date_range,
-	SUM(invoice_total) AS total_sales,
-    SUM(payment_total) AS total_payments,
-    SUM(invoice_total - payment_total) AS what_we_expect
-FROM invoices
-	WHERE invoice_date BETWEEN '2019-07-01' AND '2019-12-31'
-UNION
-SELECT
-	'Total' AS date_range,
-	SUM(invoice_total) AS total_sales,
-    SUM(payment_total) AS total_payments,
-    SUM(invoice_total - payment_total) AS what_we_expect
-FROM invoices
-	WHERE invoice_date BETWEEN '2019-01-01' AND '2019-12-31'
+	date,
+    pm.name AS payment_method,
+	SUM(amount) AS total_payments
+FROM payments p
+JOIN payment_methods pm
+	ON p.payment_method = pm.payment_method_id
+GROUP BY date, payment_method
+ORDER BY date
+
+-- SELECT
+-- 	state,
+--     city,
+-- 	SUM(invoice_total) AS total_sales
+-- FROM invoices i
+-- JOIN clients USING (client_id)
+-- GROUP BY state, city
+
+-- AGGREGATE FUNCTIONS
+
+-- SELECT
+-- 	'First half of 2019' AS date_range,
+-- 	SUM(invoice_total) AS total_sales,
+--     SUM(payment_total) AS total_payments,
+--     SUM(invoice_total - payment_total) AS what_we_expect
+-- FROM invoices
+-- 	WHERE invoice_date BETWEEN '2019-01-01' AND '2019-06-30'
+-- UNION
+-- SELECT
+-- 	'Second half of 2019' AS date_range,
+-- 	SUM(invoice_total) AS total_sales,
+--     SUM(payment_total) AS total_payments,
+--     SUM(invoice_total - payment_total) AS what_we_expect
+-- FROM invoices
+-- 	WHERE invoice_date BETWEEN '2019-07-01' AND '2019-12-31'
+-- UNION
+-- SELECT
+-- 	'Total' AS date_range,
+-- 	SUM(invoice_total) AS total_sales,
+--     SUM(payment_total) AS total_payments,
+--     SUM(invoice_total - payment_total) AS what_we_expect
+-- FROM invoices
+-- 	WHERE invoice_date BETWEEN '2019-01-01' AND '2019-12-31'
 	
-
-
 -- SELECT 
 -- 	MAX(invoice_total) AS highest,
 -- 	MIN(invoice_total) AS lowest,
