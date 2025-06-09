bcd486b5f09762df5a3067eccada95b98a29a59f
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-06-09 21:51:43 +0800

    using Parameters with default values

--- Query Test.sql
+++ Query Test.sql
@@ -1,17 +1,53 @@
--- Using Paramters in the Stored Procedures
+-- **Using Parameters with default values**
 
-DROP PROCEDURE IF EXISTS get_invoices_by_client
+DROP PROCEDURE IF EXISTS get_payments;
 
 DELIMITER $$
-CREATE PROCEDURE get_invoices_by_client
+CREATE PROCEDURE get_payments
 (
-	client_id INT
+	client_id INT,
+    payment_method_id TINYINT
 )
 BEGIN
-	SELECT * FROM invoices i
-    WHERE i.client_id = client_id;
+
+	SELECT * FROM payments p
+    WHERE p.client_id = IFNULL(client_id, p.client_id) AND
+    p.payment_method = IFNULL(payment_method_id, p.payment_method);
+    
+
 END$$
 DELIMITER ;
+
+-- DROP PROCEDURE IF EXISTS get_clients_by_state;
+
+-- DELIMITER $$
+-- CREATE PROCEDURE get_clients_by_state
+-- (
+-- 	state CHAR(2)
+-- )
+-- BEGIN
+
+-- 	SELECT * FROM clients c
+-- 	WHERE c.state = IFNULL(state, c.state);
+-- END$$
+-- DELIMITER ;
+
+
+
+-- **Using Paramters in the Stored Procedures**
+
+-- DROP PROCEDURE IF EXISTS get_invoices_by_client
+
+-- DELIMITER $$
+-- CREATE PROCEDURE get_invoices_by_client
+-- (
+-- 	client_id INT
+-- )
+-- BEGIN
+-- 	SELECT * FROM invoices i
+--     WHERE i.client_id = client_id;
+-- END$$
+-- DELIMITER ;
 
 -- DROP PROCEDURE IF EXISTS get_clients_by_state -- it's good practice to move stored procedures to separate files
 
@@ -28,7 +64,7 @@ DELIMITER ;
 
 
 
--- Dropping Stored Procedures
+-- **Dropping Stored Procedures**
 
 -- DROP PROCEDURE IF EXISTS get_clients -- it's good practice to move stored procedures to separate files
 
