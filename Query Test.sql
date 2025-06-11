0694067ec0be54a28ead9c053b5b24268f637bb1
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-06-11 23:09:59 +0800

    creating Triggers

--- Query Test.sql
+++ Query Test.sql
@@ -1,3 +1,44 @@
+-- Creating Triggers
+
+-- Creating Triggers
+
+-- Create a trigger that gets fired when we delete a payment.
+
+DELETE FROM payments
+WHERE payment_id = 10;
+
+DELIMITER $$
+CREATE TRIGGER payments_after_delete
+	AFTER DELETE ON payments
+    FOR EACH ROW
+BEGIN
+	UPDATE invoices
+    SET payment_total = payment_total - OLD.amount
+    WHERE invoice_id = OLD.invoice_id;
+END$$
+DELIMITER ;
+
+
+-- INSERT INTO payments
+-- VALUES (DEFAULT, 5, 3, '2019-01-01', 10, 1)
+
+-- DELIMITER $$
+-- CREATE TRIGGER payments_after_insert
+-- 	AFTER INSERT ON payments -- AFTER UPDATE/DELETE
+--     FOR EACH ROW
+-- BEGIN
+-- 	UPDATE invoices
+--     SET payment_total = payment_total + NEW.amount
+--     WHERE invoice_id = NEW.invoice_id;
+-- END $$
+-- DELIMITER ;
+
+-- A trigger is a block of SQL code that automatically gets executed before or after an insert, update, or delete statement.
+
+
+-- -------------------------------------------------------------------------------------------------------------------
+
+
 -- Other Conventions
 
 -- Stick to the convetions based on your company
