85b03543dec71815c172833da7bed5490933e9f1
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-06-12 23:53:39 +0800

    Concurrency and Locking

--- Query Test.sql
+++ Query Test.sql
@@ -1,17 +1,28 @@
--- Creating Transactions
+-- Concurrency and Locking
 
 USE sql_store;
 
 START TRANSACTION;
+UPDATE customers
+SET points = points + 10
+WHERE customer_id = 1;
+COMMIT; 
 
-INSERT INTO orders (customer_id, order_date, status)
-VALUES (1, '2019-01-01', 1);
 
-INSERT INTO  order_items
-VALUES (LAST_INSERT_ID(), 1, 1, 1);
 
-ROLLBACK;
--- COMMIT;
+-- Creating Transactions
+
+-- USE sql_store;
+
+-- START TRANSACTION;
+
+-- INSERT INTO orders (customer_id, order_date, status)
+-- VALUES (1, '2019-01-01', 1);
+
+-- INSERT INTO  order_items
+-- VALUES (LAST_INSERT_ID(), 1, 1, 1);
+
+-- ROLLBACK; -- or COMMIT;
 
 -- A Transaction is a group of SQL statements that represent a single unit of work.
 
