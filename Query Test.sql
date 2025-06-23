404ab85e1b3c7e86d0d9d694320db0ce052263fd
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-06-23 23:17:13 +0800

    Creating Tables

--- Query Test.sql
+++ Query Test.sql
@@ -1,5 +1,22 @@
--- Creating and Dropping Databases
+-- Creating Tables
+
 CREATE DATABASE IF NOT EXISTS sql_store2;
+USE sql_store2;
+DROP TABLE IF EXISTS customers; -- execute before CREATE to avoid getting an error in creating the table
+-- or use
+-- CREATE TABLE IF NOT EXISTS customers
+CREATE TABLE customers
+(
+	customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name 	VARCHAR(50) NOT NULL,
+    points 		INT NOT NULL DEFAULT 0,
+    email 		VARCHAR(255) NOT NULL UNIQUE
+);
+
+
+
+-- Creating and Dropping Databases
+-- CREATE DATABASE IF NOT EXISTS sql_store2;
 -- DROP DATABASE IF EXISTS sql_store2;
 
 
