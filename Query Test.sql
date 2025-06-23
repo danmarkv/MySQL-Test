75b8f355cf147b573ec8ac863953ec10063ef129
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-06-23 23:22:49 +0800

    Altering Tables

--- Query Test.sql
+++ Query Test.sql
@@ -1,17 +1,28 @@
+-- Altering Tables
+
+ALTER TABLE customers
+	ADD last_name VARCHAR(50) NOT NULL AFTER first_name,
+    ADD	city		VARCHAR(50) NOT NULL,
+    MODIFY COLUMN first_name VARCHAR(55) DEFAULT '',
+    DROP points;
+-- Remember: do not alter tables in a PRODUCTION ENVIRONMENT, always try to use TEST DATABASE to see if you alterations work AND THEN you can execute it to production database
+
+
+
 -- Creating Tables
 
-CREATE DATABASE IF NOT EXISTS sql_store2;
-USE sql_store2;
-DROP TABLE IF EXISTS customers; -- execute before CREATE to avoid getting an error in creating the table
--- or use
--- CREATE TABLE IF NOT EXISTS customers
-CREATE TABLE customers
-(
-	customer_id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name 	VARCHAR(50) NOT NULL,
-    points 		INT NOT NULL DEFAULT 0,
-    email 		VARCHAR(255) NOT NULL UNIQUE
-);
+-- CREATE DATABASE IF NOT EXISTS sql_store2;
+-- USE sql_store2;
+-- DROP TABLE IF EXISTS customers; -- execute before CREATE to avoid getting an error in creating the table
+-- -- or use
+-- -- CREATE TABLE IF NOT EXISTS customers
+-- CREATE TABLE customers
+-- (
+-- 	customer_id INT PRIMARY KEY AUTO_INCREMENT,
+--     first_name 	VARCHAR(50) NOT NULL,
+--     points 		INT NOT NULL DEFAULT 0,
+--     email 		VARCHAR(255) NOT NULL UNIQUE
+-- );
 
 
 
