4c701318843eaf6867369cfd428045d217ea1a58
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-06-23 23:46:59 +0800

    Altering Primary and Foreign Key Constraints

--- Query Test.sql
+++ Query Test.sql
@@ -1,27 +1,40 @@
+-- Altering Primary and Foreign Key Constraints
+
+ALTER TABLE orders
+	ADD PRIMARY KEY (order_id),
+    DROP PRIMARY KEY,
+	DROP FOREIGN KEY fk_orders_customers,
+    ADD FOREIGN KEY fk_orders_customers (customer_id)
+		REFERENCES customers (customer_id)
+        ON UPDATE CASCADE 
+        ON DELETE NO ACTION
+
+
+
 -- Creating Relationships
 
-CREATE DATABASE IF NOT EXISTS sql_store2;
-USE sql_store2;
-DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS customers; -- this will not be executed because the CUSTOMERS table depends on the ORDERS table. we need to move the DROP TABLE IF EXISTS orders before this line of code
+-- CREATE DATABASE IF NOT EXISTS sql_store2;
+-- USE sql_store2;
+-- DROP TABLE IF EXISTS orders;
+-- DROP TABLE IF EXISTS customers; -- this will not be executed because the CUSTOMERS table depends on the ORDERS table. we need to move the DROP TABLE IF EXISTS orders before this line of code
 
-CREATE TABLE IF NOT EXISTS customers
-(
+-- CREATE TABLE IF NOT EXISTS customers
+-- (
 
-	customer_id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name 	VARCHAR(50) NOT NULL,
-    points 		INT NOT NULL DEFAULT 0,
-    email 		VARCHAR(255) NOT NULL UNIQUE
-);
-CREATE TABLE orders
-(
-	order_id 	INT PRIMARY KEY,
-    customer_id INT NOT NULL,
-    FOREIGN KEY fk_orders_customers (customer_id)
-		REFERENCES customers (customer_id)
-        ON UPDATE CASCADE
-        ON DELETE NO ACTION
-);
+-- 	customer_id INT PRIMARY KEY AUTO_INCREMENT,
+--     first_name 	VARCHAR(50) NOT NULL,
+--     points 		INT NOT NULL DEFAULT 0,
+--     email 		VARCHAR(255) NOT NULL UNIQUE
+-- );
+-- CREATE TABLE orders
+-- (
+-- 	order_id 	INT PRIMARY KEY,
+--     customer_id INT NOT NULL,
+--     FOREIGN KEY fk_orders_customers (customer_id)
+-- 		REFERENCES customers (customer_id)
+--         ON UPDATE CASCADE
+--         ON DELETE NO ACTION
+-- );
 
 
 
