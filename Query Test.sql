3b1ae2152abdf1913aab71dfd03c892f3f470f5e
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-06-24 23:19:50 +0800

    creating Indexes

--- Query Test.sql
+++ Query Test.sql
@@ -1,3 +1,16 @@
+-- Creating Indexes
+
+-- Write a query to find customers with more than 1000 points.
+
+EXPLAIN SELECT customer_id FROM customers WHERE points > 1000;
+CREATE INDEX idx_points ON customers (points);
+
+-- EXPLAIN SELECT customer_id FROM customers WHERE state = 'CA';
+
+-- CREATE INDEX idx_state ON customers (state);
+
+
+
 -- Indexes
 -- reserve indexes for performance critical queries. don't use indexes on every column
 -- design indexes based on your queries, not the tables
